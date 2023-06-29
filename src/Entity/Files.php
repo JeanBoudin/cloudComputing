f4ba935a1f138882576f5994804bae44af74328d
Maxime <maximevaugoyeau@normandieimages.fr>
2023-06-29 09:56:35 +0200

    entity form

---
+++ src/Entity/Files.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilesRepository::class)]
+class Files
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $filename = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $upload_path = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): static
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getUploadPath(): ?string
+    {
+        return $this->upload_path;
+    }
+
+    public function setUploadPath(string $upload_path): static
+    {
+        $this->upload_path = $upload_path;
+
+        return $this;
+    }
+}
